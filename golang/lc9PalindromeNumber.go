1022495876b945ba4cfbaa7d868ce19d135cbc9e
zxw <xzhang305@gmail.com>
2022-01-04 21:23:06 +0800

    lc9

---
+++ golang/lc9PalindromeNumber.go
@@ -0,0 +1,25 @@
+package golang
+
+import "strconv"
+
+/*
+ * @lc app=leetcode.cn id=9 lang=golang
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+func isPalindrome(x int) bool {
+	if x < 0 || (x != 0 && x%10 != 0) {
+		return false
+	}
+	s := strconv.Itoa(x)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+// @lc code=end
