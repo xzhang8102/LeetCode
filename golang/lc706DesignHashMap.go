7273658f1836f6876d155d6e601d0968cb1e9bc1
zxw <xzhang305@gmail.com>
2022-01-06 23:34:16 +0800

    utilize built-in linked list

--- golang/lc706DesignHashMap.go
+++ golang/lc706DesignHashMap.go
@@ -7,73 +7,52 @@ package golang
  */
 
 // @lc code=start
-type mapNode struct {
+import "container/list"
+
+const base = 769
+
+type entry struct {
 	key, val int
-	next     *mapNode
 }
 
 type MyHashMap struct {
-	size      int
-	container []*mapNode
+	data []list.List
 }
 
 func Constructor() MyHashMap {
 	res := MyHashMap{
-		1000,
-		make([]*mapNode, 1000),
+		make([]list.List, base),
 	}
 	// 0 <= key, value <= 10^6
 	return res
 }
 
 func (this *MyHashMap) Put(key int, value int) {
-	index := key % this.size
-	head := this.container[index]
-	if head == nil {
-		this.container[index] = &mapNode{key, value, nil}
-	} else {
-		ptr := head
-		for ; ptr.key != key && ptr.next != nil; ptr = ptr.next {
-		}
-		if ptr.key != key {
-			ptr.next = &mapNode{key, value, nil}
-		} else {
-			ptr.val = value
+	index := key % base
+	for e := this.data[index].Front(); e != nil; e = e.Next() {
+		if node := e.Value.(entry); node.key == key {
+			e.Value = entry{key, value}
+			return
 		}
 	}
+	this.data[index].PushBack(entry{key, value})
 }
 
 func (this *MyHashMap) Get(key int) int {
-	index := key % this.size
-	head := this.container[index]
-	if head == nil {
-		return -1
-	} else {
-		ptr := head
-		for ; ptr.key != key && ptr.next != nil; ptr = ptr.next {
-		}
-		if ptr.key != key {
-			return -1
-		} else {
-			return ptr.val
+	index := key % base
+	for e := this.data[index].Front(); e != nil; e = e.Next() {
+		if node := e.Value.(entry); node.key == key {
+			return node.val
 		}
 	}
+	return -1
 }
 
 func (this *MyHashMap) Remove(key int) {
-	index := key % this.size
-	head := this.container[index]
-	if head != nil {
-		var prev, curr *mapNode = nil, head
-		for ; curr != nil && curr.key != key; prev, curr = curr, curr.next {
-		}
-		if curr != nil && curr.key == key {
-			if prev != nil {
-				prev.next = curr.next
-			} else {
-				this.container[index] = curr.next
-			}
-			curr.next = nil
+	index := key % base
+	for e := this.data[index].Front(); e != nil; e = e.Next() {
+		if e.Value.(entry).key == key {
+			this.data[index].Remove(e)
 		}
 	}
 }
