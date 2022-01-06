8ad3a9c0c69098879323080ae47aeacf6d43a414
zxw <xzhang305@gmail.com>
2022-01-06 22:50:53 +0800

    lc56

---
+++ golang/lc56MergeIntervals.go
@@ -0,0 +1,44 @@
+package golang
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=56 lang=golang
+ *
+ * [56] Merge Intervals
+ */
+
+// @lc code=start
+func merge(intervals [][]int) [][]int {
+	ans := [][]int{}
+	n := len(intervals)
+	if n == 0 {
+		return ans
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		loI, hiI := intervals[i][0], intervals[i][1]
+		loJ, hiJ := intervals[j][0], intervals[j][1]
+		if loI == loJ {
+			return hiI < hiJ
+		}
+		return loI < loJ
+	})
+	for i := 0; i < n; i++ {
+		lo, hi := intervals[i][0], intervals[i][1]
+		if len(ans) == 0 || ans[len(ans)-1][1] < lo {
+			ans = append(ans, []int{lo, hi})
+		} else {
+			ans[len(ans)-1][1] = lc56Max(ans[len(ans)-1][1], hi)
+		}
+	}
+	return ans
+}
+
+func lc56Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
