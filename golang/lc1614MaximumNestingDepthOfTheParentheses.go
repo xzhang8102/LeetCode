2b6539e92add877d3b14ad41e2a37aa200032db0
zxw <xzhang305@gmail.com>
2022-01-07 09:54:54 +0800

    lc1614: input is already valid

--- golang/lc1614MaximumNestingDepthOfTheParentheses.go
+++ golang/lc1614MaximumNestingDepthOfTheParentheses.go
@@ -10,29 +10,17 @@ package golang
 func maxDepth(s string) int {
 	ans := 0
 	n := len(s)
-	if n == 0 {
-		return ans
-	}
-	stack := []byte{}
-	ptr := -1 // index of left parenthesis
+	ptr := 0 // index of left parenthesis
 	for i := 0; i < n; i++ {
 		if s[i] == '(' {
-			stack = append(stack, s[i])
 			ptr++
-			if ptr+1 > ans {
-				ans = ptr + 1
+			if ptr > ans {
+				ans = ptr
 			}
 		}
 		if s[i] == ')' {
-			if len(stack) == 0 {
-				return 0
-			}
-			stack = stack[:ptr]
 			ptr--
 		}
-	}
-	if len(stack) > 0 {
-		return 0
 	}
 	return ans
 }
