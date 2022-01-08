c66dc98ff7c56ee4219c861869c7c0c351af7273
zxw <xzhang305@gmail.com>
2022-01-08 11:24:18 +0800

    lc89:backtracking

--- golang/lc89GrayCode.go
+++ golang/lc89GrayCode.go
@@ -7,13 +7,40 @@ package golang
  */
 
 // @lc code=start
+import "strconv"
+
 func grayCode(n int) []int {
-	ans := make([]int, 1, 1<<n)
-	for i := 1; i <= n; i++ {
-		for j := len(ans) - 1; j >= 0; j-- {
-			ans = append(ans, ans[j]|1<<(i-1))
+	ans := []int{0}
+	visited := map[int]bool{
+		0: true,
+	}
+	pick := make([]byte, n)
+	for i := range pick {
+		pick[i] = '0'
+	}
+	var backtrack func(index int, bit byte)
+	backtrack = func(index int, bit byte) {
+		prevBit := pick[index]
+		pick[index] = bit
+		parsed, _ := strconv.ParseInt(string(pick), 2, n+1)
+		num := int(parsed)
+		if !visited[num] {
+			ans = append(ans, num)
+			visited[num] = true
+			for i := 0; i < n; i++ {
+				if i != index {
+					if pick[i] == '1' {
+						backtrack(i, '0')
+					} else {
+						backtrack(i, '1')
+					}
+				}
+			}
+		} else {
+			pick[index] = prevBit
 		}
 	}
+	backtrack(n-1, '1')
 	return ans
 }
 
