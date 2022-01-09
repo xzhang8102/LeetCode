d633b9ee84733492460d417aee14c4ecebf70917
zxw <xzhang305@gmail.com>
2022-01-09 22:52:58 +0800

    lc40:enhanced version

--- golang/lc40CombinationSumIi.go
+++ golang/lc40CombinationSumIi.go
@@ -13,29 +13,24 @@ func combinationSum2(candidates []int, target int) [][]int {
 	ans := [][]int{}
 	pick := []int{}
 	sort.Ints(candidates)
-	visited := make([]bool, len(candidates))
 	var backtrack func(int, int)
-	backtrack = func(index, remain int) {
-		if index > len(candidates) {
-			return
-		}
-		if remain == 0 {
+	backtrack = func(begin, sum int) {
+		if sum == target {
 			ans = append(ans, append([]int(nil), pick...))
 			return
 		}
-		for i := index; i < len(candidates); i++ {
-			if remain-candidates[i] >= 0 {
-				if i == 0 || (i > 0 && candidates[i-1] != candidates[i]) || (candidates[i-1] == candidates[i] && visited[i-1]) {
-					visited[i] = true
-					pick = append(pick, candidates[i])
-					backtrack(i+1, remain-candidates[i])
-					pick = pick[:len(pick)-1]
-					visited[i] = false
-				}
+		for i := begin; i < len(candidates); i++ {
+			if i > begin && candidates[i] == candidates[i-1] {
+				continue
+			}
+			if candidates[i]+sum <= target {
+				pick = append(pick, candidates[i])
+				backtrack(i+1, candidates[i]+sum)
+				pick = pick[:len(pick)-1]
 			}
 		}
 	}
-	backtrack(0, target)
+	backtrack(0, 0)
 	return ans
 }
 
