e046bb245fa09e535bea05e4121ce7d52c17ebd1
zxw <xzhang305@gmail.com>
2022-01-13 17:25:08 +0800

    lc413: math(100%, 71.43%)

--- golang/lc413ArithmeticSlices.go
+++ golang/lc413ArithmeticSlices.go
@@ -13,7 +13,7 @@ func numberOfArithmeticSlices(nums []int) int {
 		return 0
 	}
 	ans := 0
-	gap, count := nums[0]-nums[1], 0
+	gap, length := nums[0]-nums[1], 2
 	// 因为等差数列的长度至少为 3，所以可以从 i=2 开始枚举
 	/*
 		input: [1,2,3,4,5]
@@ -23,11 +23,16 @@ func numberOfArithmeticSlices(nums []int) int {
 	*/
 	for i := 2; i < n; i++ {
 		if nums[i-1]-nums[i] == gap {
-			count++
+			length++
 		} else {
-			gap, count = nums[i-1]-nums[i], 0
+			if length > 2 {
+				ans += (length - 1) * (length - 2) / 2
+			}
+			gap, length = nums[i-1]-nums[i], 2
 		}
-		ans += count
+	}
+	if length > 2 {
+		ans += (length - 1) * (length - 2) / 2
 	}
 	return ans
 }
