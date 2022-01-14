a46a7249e4c8f745083b7d26e846c5bc960471f0
zxw <xzhang305@gmail.com>
2022-01-14 10:47:50 +0800

    lc139: dp no map(100%, 98.37%)

--- golang/lc139WordBreak.go
+++ golang/lc139WordBreak.go
@@ -8,24 +8,19 @@ package golang
 
 // @lc code=start
 func wordBreak(s string, wordDict []string) bool {
-	n := len(s)
-	dict := map[string]bool{}
-	for _, str := range wordDict {
-		dict[str] = true
-	}
-	dp := make([]bool, n+1)
+	dp := make([]bool, len(s)+1)
 	dp[0] = true
-	for i := 1; i <= n; i++ {
-		valid := false
-		for j := i - 1; j >= 0; j-- {
-			if dp[j] && dict[s[j:i]] {
-				valid = true
+	for i := 1; i <= len(s); i++ {
+		for _, target := range wordDict {
+			if i >= len(target) &&
+				s[i-len(target):i] == target &&
+				dp[i-len(target)] {
+				dp[i] = true
 				break
 			}
 		}
-		dp[i] = valid
 	}
-	return dp[n]
+	return dp[len(s)]
 }
 
 // @lc code=end
