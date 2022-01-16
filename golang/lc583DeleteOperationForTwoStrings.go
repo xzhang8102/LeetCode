83c9d0a3c8743fc15bf16a6ff2f8953d4b26d28b
zxw <xzhang305@gmail.com>
2022-01-16 15:01:07 +0800

    lc583: dp top down(87.87%, 33.44%)

--- golang/lc583DeleteOperationForTwoStrings.go
+++ golang/lc583DeleteOperationForTwoStrings.go
@@ -9,32 +9,26 @@ package golang
 // @lc code=start
 func minDistance(word1 string, word2 string) int {
 	len1, len2 := len(word1), len(word2)
-	memo := make([][]int, len1)
-	for i := range memo {
-		memo[i] = make([]int, len2)
-		for k := range memo[i] {
-			memo[i][k] = -1
+	dp := make([][]int, len1+1)
+	for i := range dp {
+		dp[i] = make([]int, len2+1)
+	}
+	for i := 0; i <= len1; i++ {
+		dp[i][0] = i
+	}
+	for i := range dp[0] {
+		dp[0][i] = i
+	}
+	for index1 := 1; index1 <= len1; index1++ {
+		for index2 := 1; index2 <= len2; index2++ {
+			if word1[index1-1] == word2[index2-1] {
+				dp[index1][index2] = dp[index1-1][index2-1]
+			} else {
+				dp[index1][index2] = 1 + lc583Min(dp[index1-1][index2], dp[index1][index2-1])
+			}
 		}
 	}
-	var dp func(int, int) int
-	dp = func(index1, index2 int) int {
-		if index1 == len1 {
-			return len2 - index2
-		}
-		if index2 == len2 {
-			return len1 - index1
-		}
-		if memo[index1][index2] != -1 {
-			return memo[index1][index2]
-		}
-		if word1[index1] != word2[index2] {
-			memo[index1][index2] = 1 + lc583Min(dp(index1+1, index2), dp(index1, index2+1))
-		} else {
-			memo[index1][index2] = dp(index1+1, index2+1)
-		}
-		return memo[index1][index2]
-	}
-	return dp(0, 0)
+	return dp[len1][len2]
 }
 
 func lc583Min(a, b int) int {
