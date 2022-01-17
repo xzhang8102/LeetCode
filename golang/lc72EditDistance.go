bf7ded289a51b1811f65334c9d0940473ef952e7
zxw <xzhang305@gmail.com>
2022-01-17 22:49:06 +0800

    lc72: dp top down(43.84%, 55.31%)

--- golang/lc72EditDistance.go
+++ golang/lc72EditDistance.go
@@ -9,46 +9,39 @@ package golang
 // @lc code=start
 func minDistance(word1 string, word2 string) int {
 	len1, len2 := len(word1), len(word2)
-	memo := make([][]int, len1)
-	for i := range memo {
-		memo[i] = make([]int, len2)
-		for j := range memo[i] {
-			memo[i][j] = -1
+	dp := make([][]int, len1+1)
+	for i := range dp {
+		dp[i] = make([]int, len2+1)
+		dp[i][0] = i
+	}
+	for i := range dp[0] {
+		dp[0][i] = i
+	}
+	for i1 := 1; i1 <= len1; i1++ {
+		for i2 := 1; i2 <= len2; i2++ {
+			if word1[i1-1] == word2[i2-1] {
+				dp[i1][i2] = dp[i1-1][i2-1]
+			} else {
+				dp[i1][i2] = lc72Min(
+					// replace current char
+					dp[i1-1][i2-1]+1,
+					// add the diff char
+					lc72Min(
+						dp[i1-1][i2]+1,
+						dp[i1][i2-1]+1,
+					),
+				)
+			}
 		}
 	}
-	var dp func(int, int) int
-	dp = func(i1, i2 int) int {
-		if i1 == len1 {
-			return len2 - i2
-		}
-		if i2 == len2 {
-			return len1 - i1
-		}
-		if memo[i1][i2] != -1 {
-			return memo[i1][i2]
-		}
-		if word1[i1] == word2[i2] {
-			memo[i1][i2] = dp(i1+1, i2+1)
-		} else {
-			memo[i1][i2] = lc72Min(
-				1+dp(i1+1, i2+1),
-				1+dp(i1+1, i2),
-				1+dp(i1, i2+1),
-			)
-		}
-		return memo[i1][i2]
-	}
-	return dp(0, 0)
+	return dp[len1][len2]
 }
 
-func lc72Min(nums ...int) int {
-	min := nums[0]
-	for _, v := range nums {
-		if v < min {
-			min = v
-		}
+func lc72Min(a, b int) int {
+	if a < b {
+		return a
 	}
-	return min
+	return b
 }
 
 // @lc code=end
