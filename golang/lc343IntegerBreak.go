676be94dc812d7caf2acd6e398ab30a40d3017e0
zxw <xzhang305@gmail.com>
2022-01-18 11:24:49 +0800

    lc343: update transition function(100%, 68.13%)

--- golang/lc343IntegerBreak.go
+++ golang/lc343IntegerBreak.go
@@ -12,22 +12,19 @@ func integerBreak(n int) int {
 	dp[1] = 0
 	for i := 2; i <= n; i++ {
 		max := 0
-		for j := i - 1; j >= (i+1)/2; j-- {
-			max = lc343Max(max, (i-j)*j, dp[i-j]*dp[j], dp[i-j]*j, (i-j)*dp[j])
+		for j := i - 1; j >= 1; j-- {
+			max = lc343Max(max, lc343Max((i-j)*j, dp[i-j]*j))
 		}
 		dp[i] = max
 	}
 	return dp[n]
 }
 
-func lc343Max(a ...int) int {
-	max := a[0]
-	for _, v := range a[1:] {
-		if v > max {
-			max = v
-		}
+func lc343Max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return max
+	return b
 }
 
 // @lc code=end
