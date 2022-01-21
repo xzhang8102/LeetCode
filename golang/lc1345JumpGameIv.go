cee92539a91a75320ef600dca78b34f7cdb4f21b
zxw <xzhang305@gmail.com>
2022-01-21 14:36:00 +0800

    lc1345: bfs(30.23%, 55.81%)

--- golang/lc1345JumpGameIv.go
+++ golang/lc1345JumpGameIv.go
@@ -1,7 +1,5 @@
 package golang
 
-import "math"
-
 /*
  * @lc app=leetcode.cn id=1345 lang=golang
  *
@@ -16,37 +14,33 @@ func minJumps(arr []int) int {
 		mapping[v] = append(mapping[v], i)
 	}
 	visited := map[int]bool{0: true} // visited index
-	ans, count := math.MaxInt32, 0
-	var backtrack func(int)
-	backtrack = func(index int) {
-		if index == n-1 {
-			if count < ans {
-				ans = count
+	type pair struct {
+		idx, step int
+	}
+	q := []pair{{0, 0}}
+	for len(q) > 0 {
+		point := q[0]
+		q = q[1:]
+		if point.idx == n-1 {
+			return point.step
+		}
+		for _, pos := range mapping[arr[point.idx]] {
+			if !visited[pos] {
+				visited[pos] = true
+				q = append(q, pair{pos, point.step + 1})
 			}
-			return
 		}
-		positions := map[int]bool{}
-		if index < n-1 {
-			positions[index+1] = true
+		delete(mapping, arr[point.idx])
+		if !visited[point.idx+1] {
+			visited[point.idx+1] = true
+			q = append(q, pair{point.idx + 1, point.step + 1})
 		}
-		for _, pos := range mapping[arr[index]] {
-			positions[pos] = true
-		}
-		if index > 0 {
-			positions[index-1] = true
-		}
-		for next := range positions {
-			if !visited[next] && count < ans {
-				count++
-				visited[next] = true
-				backtrack(next)
-				count--
-				visited[next] = false
-			}
+		if point.idx > 0 && !visited[point.idx-1] {
+			visited[point.idx-1] = true
+			q = append(q, pair{point.idx - 1, point.step + 1})
 		}
 	}
-	backtrack(0)
-	return ans
+	return -1
 }
 
 // @lc code=end
