94838326caf5f4f5dff6620488e301b2d66db59b
zxw <xzhang305@gmail.com>
2022-01-28 00:01:03 +0800

    lc42: stack(54.73%, 99.47%)

--- golang/lc42TrappingRainWater.go
+++ golang/lc42TrappingRainWater.go
@@ -13,17 +13,19 @@ func trap(height []int) int {
 		return 0
 	}
 	ans := 0
-	leftMax, rightMax := make([]int, n), make([]int, n)
-	leftMax[0] = height[0]
-	for i := 1; i < n; i++ {
-		leftMax[i] = lc42Max(leftMax[i-1], height[i])
-	}
-	rightMax[n-1] = height[n-1]
-	for i := n - 2; i >= 0; i-- {
-		rightMax[i] = lc42Max(rightMax[i+1], height[i])
-	}
+	stack := []int{}
 	for i, h := range height {
-		ans += lc42Min(leftMax[i], rightMax[i]) - h
+		for len(stack) > 0 && h >= height[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				break
+			}
+			left := stack[len(stack)-1]
+			width := i - left - 1
+			ans += (lc42Min(height[left], h) - height[top]) * width
+		}
+		stack = append(stack, i)
 	}
 	return ans
 }
