b25d93034d097639b065514c6d98ad34aa9e15c9
zxw <xzhang305@gmail.com>
2022-01-29 10:57:26 +0800

    lc1765: bfs(91.67%, 16.67%)

---
+++ golang/lc1765MapOfHighestPeak.go
@@ -0,0 +1,64 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=1765 lang=golang
+ *
+ * [1765] Map of Highest Peak
+ */
+
+// @lc code=start
+func highestPeak(isWater [][]int) [][]int {
+	row, col := len(isWater), len(isWater[0])
+	ans := make([][]int, row)
+	for r := range ans {
+		ans[r] = make([]int, col)
+		for c := range ans[r] {
+			ans[r][c] = -1
+		}
+	}
+	type pair struct{ row, col int }
+	q := []pair{}
+	for r := 0; r < row; r++ {
+		for c := 0; c < col; c++ {
+			if isWater[r][c] == 1 {
+				ans[r][c] = 0
+				q = append(q, pair{r, c})
+			}
+		}
+	}
+	directions := [4][2]int{
+		{1, 0},
+		{-1, 0},
+		{0, 1},
+		{0, -1},
+	}
+	for len(q) > 0 {
+		tmp := q
+		q = []pair{}
+		for _, p := range tmp {
+			r, c := p.row, p.col
+			h := ans[r][c]
+			for _, dir := range directions {
+				newR, newC := r+dir[0], c+dir[1]
+				if newR >= 0 && newR < row && newC >= 0 && newC < col {
+					if ans[newR][newC] == -1 {
+						ans[newR][newC] = h + 1
+						q = append(q, pair{newR, newC})
+					} else {
+						ans[newR][newC] = lc1765Min(ans[newR][newC], h+1)
+					}
+				}
+			}
+		}
+	}
+	return ans
+}
+
+func lc1765Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
