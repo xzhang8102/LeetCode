b4a9d9c0797f4301e5c42c9d583340e6d9ffbf00
zxw <xzhang305@gmail.com>
2022-02-07 10:43:39 +0800

    lc1405: greedy(100%, 86.96%)

--- golang/lc1405LongestHappyString.go
+++ golang/lc1405LongestHappyString.go
@@ -1,5 +1,7 @@
 package golang
 
+import "container/heap"
+
 /*
  * @lc app=leetcode.cn id=1405 lang=golang
  *
@@ -7,38 +9,57 @@ package golang
  */
 
 // @lc code=start
+type charPair struct {
+	char byte
+	cnt  int
+}
+
+type lc1405PQ []charPair
+
+func (pq *lc1405PQ) Len() int           { return len(*pq) }
+func (pq *lc1405PQ) Less(i, j int) bool { return (*pq)[i].cnt > (*pq)[j].cnt }
+func (pq *lc1405PQ) Swap(i, j int) {
+	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
+}
+func (pq *lc1405PQ) Pop() interface{} {
+	item := (*pq)[pq.Len()-1]
+	*pq = (*pq)[:pq.Len()-1]
+	return item
+}
+func (pq *lc1405PQ) Push(x interface{}) {
+	*pq = append(*pq, x.(charPair))
+}
+
 func longestDiverseString(a int, b int, c int) string {
-	ans := ""
-	path := []byte{}
-	var backtrack func(aCnt, bCnt, cCnt int)
-	backtrack = func(aCnt, bCnt, cCnt int) {
-		if len(path) > len(ans) {
-			ans = string(path)
-		}
-		if aCnt < a {
-			if len(path) < 2 || (len(path) >= 2 && string(path[len(path)-2:]) != "aa") {
-				path = append(path, 'a')
-				backtrack(aCnt+1, bCnt, cCnt)
-				path = path[:len(path)-1]
+	var pq lc1405PQ
+	pq = append(pq, charPair{'a', a})
+	pq = append(pq, charPair{'b', b})
+	pq = append(pq, charPair{'c', c})
+	heap.Init(&pq)
+	ans := []byte{}
+	for pq.Len() > 0 {
+		curr := heap.Pop(&pq).(charPair)
+		n := len(ans)
+		if n >= 2 && ans[n-2] == curr.char && ans[n-1] == curr.char {
+			if pq.Len() == 0 {
+				break
 			}
-		}
-		if bCnt < b {
-			if len(path) < 2 || (len(path) >= 2 && string(path[len(path)-2:]) != "bb") {
-				path = append(path, 'b')
-				backtrack(aCnt, bCnt+1, cCnt)
-				path = path[:len(path)-1]
+			next := heap.Pop(&pq).(charPair)
+			if next.cnt > 0 {
+				ans = append(ans, next.char)
+				next.cnt--
+				heap.Push(&pq, next)
 			}
-		}
-		if cCnt < c {
-			if len(path) < 2 || (len(path) >= 2 && string(path[len(path)-2:]) != "cc") {
-				path = append(path, 'c')
-				backtrack(aCnt, bCnt, cCnt+1)
-				path = path[:len(path)-1]
+			heap.Push(&pq, curr)
+		} else {
+			if curr.cnt > 0 {
+				ans = append(ans, curr.char)
+				curr.cnt--
+				heap.Push(&pq, curr)
 			}
 		}
 	}
-	backtrack(0, 0, 0)
-	return ans
+	return string(ans)
 }
 
 // @lc code=end
