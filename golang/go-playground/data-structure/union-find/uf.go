896200fa81a94d2ed2f2b905c28a0d76116f5f6f
zxw <xzhang305@gmail.com>
2022-02-07 16:09:51 +0800

    add union find (with path compression)

---
+++ golang/go-playground/data-structure/union-find/uf.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type UnionFind struct {
+	parents     []int
+	Connections int
+}
+
+func NewUnionFind(size int) *UnionFind {
+	parents := make([]int, size)
+	for i := range parents {
+		parents[i] = -1 // every node is its root and the size of the tree is abs(-1)
+	}
+	return &UnionFind{parents, size}
+}
+
+func (uf *UnionFind) find(val int) int {
+	path := []int{}
+	for uf.parents[val] >= 0 {
+		path = append(path, val)
+		val = uf.parents[val]
+	}
+	// now `val` is parent's index
+	for index := range path { // path compression
+		uf.parents[index] = val
+	}
+	return val
+}
+
+func (uf *UnionFind) Union(a, b int) {
+	rootA, rootB := uf.find(a), uf.find(b)
+	if rootA != rootB {
+		sizeA, sizeB := -uf.parents[rootA], -uf.parents[rootB]
+		if sizeA >= sizeB {
+			uf.parents[rootB] = rootA
+			uf.parents[rootA] -= sizeB
+		} else {
+			uf.parents[rootA] = rootB
+			uf.parents[rootB] -= sizeA
+		}
+		uf.Connections--
+	}
+}
+
+func (uf *UnionFind) isConnected(a, b int) bool {
+	return uf.find(a) == uf.find(b)
+}
+
+func main() {
+	uf := NewUnionFind(10) // 0..9
+	uf.Union(0, 1)
+	uf.Union(1, 2)
+	uf.Union(2, 3)
+	uf.Union(3, 9)
+	fmt.Println(uf.isConnected(0, 9))
+	fmt.Println(uf.Connections)
+}
