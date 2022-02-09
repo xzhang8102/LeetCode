f5794ca2e55582cbd8d031dbfb7a49ff80c38b80
zxw <xzhang305@gmail.com>
2022-02-09 10:58:27 +0800

    lc765: union find(100%, 100%)

--- golang/lc765CouplesHoldingHands.go
+++ golang/lc765CouplesHoldingHands.go
@@ -1,26 +1,26 @@
 package golang
 
 /*
- * @lc app=leetcode.cn id=1319 lang=golang
+ * @lc app=leetcode.cn id=765 lang=golang
  *
- * [1319] Number of Operations to Make Network Connected
+ * [765] Couples Holding Hands
  */
 
 // @lc code=start
-type lc1319UF struct {
+type lc765UF struct {
 	parents     []int
 	connections int
 }
 
-func newLC1319UF(size int) *lc1319UF {
-	parents := make([]int, size)
+func newLC765UF(n int) *lc765UF {
+	parents := make([]int, n)
 	for i := range parents {
 		parents[i] = -1
 	}
-	return &lc1319UF{parents, size}
+	return &lc765UF{parents, n}
 }
 
-func (uf *lc1319UF) find(x int) int {
+func (uf *lc765UF) find(x int) int {
 	if uf.parents[x] >= 0 {
 		uf.parents[x] = uf.find(uf.parents[x])
 		return uf.parents[x]
@@ -28,7 +28,7 @@ func (uf *lc1319UF) find(x int) int {
 	return x
 }
 
-func (uf *lc1319UF) union(a, b int) {
+func (uf *lc765UF) union(a, b int) {
 	rootA, rootB := uf.find(a), uf.find(b)
 	if rootA != rootB {
 		sizeA, sizeB := -uf.parents[rootA], -uf.parents[rootB]
@@ -43,15 +43,15 @@ func (uf *lc1319UF) union(a, b int) {
 	}
 }
 
-func makeConnected(n int, connections [][]int) int {
-	if len(connections) < n-1 {
-		return -1
+func minSwapsCouples(row []int) int {
+	n := len(row)
+	uf := newLC765UF(n / 2)
+	for i := 0; i < n; i += 2 {
+		// if the adjacent two ids are a couple
+		// row[i] / 2 == row[i+1] / 2
+		uf.union(row[i]/2, row[i+1]/2)
 	}
-	uf := newLC1319UF(n)
-	for _, conn := range connections {
-		uf.union(conn[0], conn[1])
-	}
-	return uf.connections - 1
+	return n/2 - uf.connections
 }
 
 // @lc code=end
