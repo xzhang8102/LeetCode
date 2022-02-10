e1ec1957045e43d27f527d5469f84cf7d96a07a3
zxw <xzhang305@gmail.com>
2022-02-10 17:44:44 +0800

    add pooling, drop, cancellation patterns

--- golang/go-playground/go-notes/concurrency/channel-patterns/main.go
+++ golang/go-playground/go-notes/concurrency/channel-patterns/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -53,6 +55,76 @@ func waitForTask() {
 	time.Sleep(time.Second)
 }
 
+func pooling() {
+	ch := make(chan string)
+	grs := runtime.GOMAXPROCS(0) // query available thread numbers
+	for g := 0; g < grs; g++ {
+		go func(child int) {
+			for d := range ch {
+				fmt.Printf("child %d: recv'd signal: %s\n", child, d)
+			}
+			fmt.Printf("child %d: recv'd shutdown signal\n", child)
+		}(g)
+	}
+
+	const work = 100
+	for w := 0; w < work; w++ {
+		ch <- "data"
+		fmt.Println("parent: sent signal ", w)
+	}
+	close(ch)
+	fmt.Println("parent: sent shutdown signal")
+	time.Sleep(time.Second)
+}
+
+func drop() {
+	const cap = 100
+	ch := make(chan string, cap)
+
+	go func() {
+		for p := range ch {
+			fmt.Println("child : recv'd signal :", p)
+		}
+	}()
+
+	const work = 2000
+	for w := 0; w < work; w++ {
+		select {
+		case ch <- "data":
+			fmt.Println("parent : sent signal :", w)
+		default:
+			fmt.Println("parent : dropped data :", w)
+		}
+	}
+	close(ch)
+	fmt.Println("parent : sent shutdown signal")
+	time.Sleep(time.Second)
+}
+
+func cancellation() {
+	duration := 100 * time.Millisecond
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+
+	ch := make(chan string, 1)
+	go func() {
+		// The child Goroutine needs to be able to send its signal,
+		// whether or not the parent Goroutine is around to receive it.
+		// If a non-buffered channel is used,
+		// the child Goroutine will block forever and become a memory leak.
+		time.Sleep(time.Duration(rand.Intn(5000)) * time.Millisecond)
+		ch <- "data"
+	}()
+
+	select {
+	case d := <-ch:
+		fmt.Println("work complete", d)
+	case <-ctx.Done():
+		fmt.Println("work cancelled")
+	}
+	time.Sleep(time.Second)
+}
+
 func main() {
-	waitForTask()
+	cancellation()
 }
