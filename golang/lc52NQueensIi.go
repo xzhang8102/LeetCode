dd67a582ddfc21bc8a712b988acb9bd59f2aaf9c
zxw <xzhang305@gmail.com>
2022-02-11 17:49:27 +0800

    lc52: backtrack(100%, 95.74%)

--- golang/lc52NQueensIi.go
+++ golang/lc52NQueensIi.go
@@ -10,30 +10,25 @@ package golang
 func totalNQueens(n int) int {
 	col, left, right := map[int]int{}, map[int]int{}, map[int]int{}
 	ans := 0
-	pick := make([]int, 0, n)
-	var backtrack func(r, c int)
-	backtrack = func(r, c int) {
-		if len(pick) == n-1 {
+	var backtrack func(r int)
+	backtrack = func(r int) {
+		if r == n {
 			ans++
 			return
 		}
-		pick = append(pick, c)
-		col[c]++
-		left[r-c]++
-		right[r+c]++
-		for newR, newC := r+1, 0; newC < n && newR < n; newC++ {
-			if col[newC] == 0 && left[newR-newC] == 0 && right[newR+newC] == 0 {
-				backtrack(newR, newC)
+		for c := 0; c < n; c++ {
+			if col[c] == 0 && left[r-c] == 0 && right[r+c] == 0 {
+				col[c]++
+				left[r-c]++
+				right[r+c]++
+				backtrack(r + 1)
+				col[c]--
+				left[r-c]--
+				right[r+c]--
 			}
 		}
-		col[c]--
-		left[r-c]--
-		right[r+c]--
-		pick = pick[:len(pick)-1]
 	}
-	for i := 0; i < n; i++ {
-		backtrack(0, i)
-	}
+	backtrack(0)
 	return ans
 }
 
