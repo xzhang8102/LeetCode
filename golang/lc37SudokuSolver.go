40a5b2c6571655779973439bd854add9a04d784f
zxw <xzhang305@gmail.com>
2022-02-12 12:20:58 +0800

    lc37: backtrack(100%, 86.56%)

--- golang/lc37SudokuSolver.go
+++ golang/lc37SudokuSolver.go
@@ -8,7 +8,7 @@ package golang
 
 // @lc code=start
 func solveSudoku(board [][]byte) {
-	totalSlots, row, col, block := 0, [9][9]bool{}, [9][9]bool{}, [9][9]bool{}
+	slots, row, col, block := [][]int{}, [9][9]bool{}, [9][9]bool{}, [9][9]bool{}
 	for r := 0; r < 9; r++ {
 		for c := 0; c < 9; c++ {
 			char := board[r][c]
@@ -18,46 +18,35 @@ func solveSudoku(board [][]byte) {
 				col[c][char-'1'] = true
 				block[blockId][char-'1'] = true
 			} else {
-				totalSlots++
+				slots = append(slots, []int{r, c})
 			}
 		}
 	}
-	var backtrack func(int, int)
-	filled := 0
-	backtrack = func(r, c int) {
-		if board[r][c] == '.' {
-			blockId := (r/3)*3 + c/3
-			for char := byte('9'); char > '0'; char-- {
-				if !row[r][char-'1'] && !col[c][char-'1'] && !block[blockId][char-'1'] {
-					board[r][c] = char
-					row[r][char-'1'] = true
-					col[c][char-'1'] = true
-					block[blockId][char-'1'] = true
-					filled++
-					backtrack(r, c)
-					if filled == totalSlots {
-						return
-					}
-					board[r][c] = '.'
-					row[r][char-'1'] = false
-					col[c][char-'1'] = false
-					block[blockId][char-'1'] = false
-					filled--
+	var backtrack func(int) bool
+	backtrack = func(pos int) bool {
+		if pos == len(slots) {
+			return true
+		}
+		r, c := slots[pos][0], slots[pos][1]
+		blockId := (r/3)*3 + c/3
+		for char := byte('9'); char > '0'; char-- {
+			if !row[r][char-'1'] && !col[c][char-'1'] && !block[blockId][char-'1'] {
+				board[r][c] = char
+				row[r][char-'1'] = true
+				col[c][char-'1'] = true
+				block[blockId][char-'1'] = true
+				if backtrack(pos + 1) {
+					return true
 				}
+				board[r][c] = '.'
+				row[r][char-'1'] = false
+				col[c][char-'1'] = false
+				block[blockId][char-'1'] = false
 			}
-		} else {
-			if filled == totalSlots {
-				return
-			}
-			newR, newC := r, c+1
-			if newC > 8 {
-				newR++
-				newC = 0
-			}
-			backtrack(newR, newC)
 		}
+		return false
 	}
-	backtrack(0, 0)
+	backtrack(0)
 }
 
 // @lc code=end
