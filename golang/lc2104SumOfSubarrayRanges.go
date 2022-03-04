3dcb233a8072d6015a40f53aac2c8ffeac39e5ec
zxw <xzhang305@gmail.com>
2022-03-04 15:37:29 +0800

    [daily] lc2104: stack

--- golang/lc2104SumOfSubarrayRanges.go
+++ golang/lc2104SumOfSubarrayRanges.go
@@ -1,5 +1,7 @@
 package golang
 
+import "container/list"
+
 /*
  * @lc app=leetcode.cn id=2104 lang=golang
  *
@@ -9,20 +11,67 @@ package golang
 // @lc code=start
 func subArrayRanges(nums []int) int64 {
 	n := len(nums)
-	var ans int64 = 0
+	minRight := make([]int, n)
+	stack := list.New()
 	for i := 0; i < n; i++ {
-		min, max := nums[i], nums[i]
-		for j := i + 1; j < n; j++ {
-			if nums[j] > max {
-				max = nums[j]
-			}
-			if nums[j] < min {
-				min = nums[j]
-			}
-			ans += int64(max - min)
+		for top := stack.Back(); stack.Len() > 0 && nums[i] < nums[top.Value.(int)]; top = stack.Back() {
+			minRight[top.Value.(int)] = i - top.Value.(int)
+			stack.Remove(top)
+		}
+		stack.PushBack(i)
+	}
+	lc2104ClearStack(stack, minRight)
+	maxRight := make([]int, n)
+	for i := 0; i < n; i++ {
+		for top := stack.Back(); stack.Len() > 0 && nums[i] > nums[top.Value.(int)]; top = stack.Back() {
+			maxRight[top.Value.(int)] = i - top.Value.(int)
+			stack.Remove(top)
+		}
+		stack.PushBack(i)
+	}
+	lc2104ClearStack(stack, maxRight)
+	minLeft := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for top := stack.Back(); stack.Len() > 0 && nums[i] <= nums[top.Value.(int)]; top = stack.Back() {
+			minLeft[top.Value.(int)] = top.Value.(int) - i
+			stack.Remove(top)
+		}
+		stack.PushBack(i)
+	}
+	lc2104ClearStack(stack, minLeft)
+	maxLeft := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for top := stack.Back(); stack.Len() > 0 && nums[i] >= nums[top.Value.(int)]; top = stack.Back() {
+			maxLeft[top.Value.(int)] = top.Value.(int) - i
+			stack.Remove(top)
+		}
+		stack.PushBack(i)
+	}
+	lc2104ClearStack(stack, maxLeft)
+	var minSum, maxSum int64
+	for i := 0; i < n; i++ {
+		minSum += int64(minLeft[i] * minRight[i] * nums[i])
+		maxSum += int64(maxLeft[i] * maxRight[i] * nums[i])
+	}
+	return maxSum - minSum
+}
+
+func lc2104ClearStack(stack *list.List, storage []int) {
+	if stack.Len() > 0 {
+		index := stack.Back().Value.(int)
+		for stack.Len() > 0 {
+			top := stack.Back()
+			storage[top.Value.(int)] = lc2104Abs(index-top.Value.(int)) + 1
+			stack.Remove(top)
 		}
 	}
-	return ans
+}
+
+func lc2104Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 // @lc code=end
