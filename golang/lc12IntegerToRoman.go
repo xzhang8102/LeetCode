e7a80a630f5ca1a0fff8039d614438f3a83beda1
zxw <xzhang305@gmail.com>
2022-03-04 17:05:28 +0800

    [first200] lc12: string (cleanup)

--- golang/lc12IntegerToRoman.go
+++ golang/lc12IntegerToRoman.go
@@ -10,42 +10,17 @@ import "strings"
 
 // @lc code=start
 func intToRoman(num int) string {
-	mapping := map[int]rune{
-		1:    'I',
-		5:    'V',
-		10:   'X',
-		50:   'L',
-		100:  'C',
-		500:  'D',
-		1000: 'M',
+	bases := []struct {
+		val   int
+		roman string
+	}{
+		{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
 	}
-	bases := []int{1000, 500, 100, 50, 10, 5, 1}
 	var b strings.Builder
 	for _, base := range bases {
-		if quotient := num / base; quotient >= 1 {
-			switch {
-			case base == 500 && num >= 900:
-				b.WriteString("CM")
-				num -= 900
-			case base == 100 && num >= 400:
-				b.WriteString("CD")
-				num -= 400
-			case base == 50 && num >= 90:
-				b.WriteString("XC")
-				num -= 90
-			case base == 10 && num >= 40:
-				b.WriteString("XL")
-				num -= 40
-			case base == 5 && num == 9:
-				b.WriteString("IX")
-				num -= 9
-			case base == 1 && num == 4:
-				b.WriteString("IV")
-				num -= 4
-			default:
-				b.WriteString(strings.Repeat(string(mapping[base]), quotient))
-				num %= base
-			}
+		for num >= base.val {
+			b.WriteString(base.roman)
+			num -= base.val
 		}
 		if num == 0 {
 			break
