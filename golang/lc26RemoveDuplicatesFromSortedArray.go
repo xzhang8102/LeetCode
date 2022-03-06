f3f42c4a73d89a3f941312a9b510155fb7303d04
zxw <xzhang305@gmail.com>
2022-03-06 21:54:36 +0800

    [1-200] lc26: double ptr

---
+++ golang/lc26RemoveDuplicatesFromSortedArray.go
@@ -0,0 +1,24 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	n := len(nums)
+	slow, fast := 0, 1
+	for fast < n {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		} else {
+			fast++
+		}
+	}
+	return slow + 1
+}
+
+// @lc code=end
