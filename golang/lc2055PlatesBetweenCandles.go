b1973fe493fe9ee63a78acb89c5e849300f6b0c4
zxw <xzhang305@gmail.com>
2022-03-08 11:49:43 +0800

    [daily] lc2055: prefix sum; binary search

--- golang/lc2055PlatesBetweenCandles.go
+++ golang/lc2055PlatesBetweenCandles.go
@@ -1,5 +1,7 @@
 package golang
 
+import "sort"
+
 /*
  * @lc app=leetcode.cn id=2055 lang=golang
  *
@@ -11,39 +13,43 @@ func platesBetweenCandles(s string, queries [][]int) []int {
 	n := len(s)
 	// assume the left end is '|'
 	sum := make([]int, n+1)
-	// index of the left closest '|'; index of the right closest '|'
-	left, right := make([]int, n), make([]int, n)
+	candleIdx := []int{}
+	ans := make([]int, len(queries))
 	for i := 0; i < n; i++ {
 		if s[i] == '*' {
 			sum[i+1] = sum[i] + 1
-			if i == 0 {
-				left[i] = -1
-			} else {
-				left[i] = left[i-1]
-			}
 		} else {
+			candleIdx = append(candleIdx, i)
 			sum[i+1] = sum[i]
-			left[i] = i
 		}
 	}
-	for i := n - 1; i >= 0; i-- {
-		if s[i] == '*' {
-			if i == n-1 {
-				right[i] = -1
+	if len(candleIdx) == 0 {
+		return ans
+	}
+	for idx, query := range queries {
+		// find the closest '|'
+		leftIdx := sort.SearchInts(candleIdx, query[0])
+		if leftIdx == len(candleIdx) {
+			ans[idx] = 0
+			continue
+		}
+		left := candleIdx[leftIdx]
+		rightIdx := sort.SearchInts(candleIdx, query[1])
+		right := query[1]
+		if rightIdx == len(candleIdx) {
+			right = candleIdx[rightIdx-1]
+		} else if candleIdx[rightIdx] != right {
+			if rightIdx == 0 {
+				ans[idx] = 0
+				continue
 			} else {
-				right[i] = right[i+1]
+				right = candleIdx[rightIdx-1]
 			}
-		} else {
-			right[i] = i
 		}
-	}
-	ans := []int{}
-	for _, query := range queries {
-		l, r := right[query[0]], left[query[1]]
-		if l <= r && l >= query[0] && r <= query[1] {
-			ans = append(ans, sum[r+1]-sum[l+1])
+		if left <= right {
+			ans[idx] = sum[right] - sum[left]
 		} else {
-			ans = append(ans, 0)
+			ans[idx] = 0
 		}
 	}
 	return ans
