fc1286a9f0038f251c43e36fe9044ae12d2d3232
zxw <xzhang305@gmail.com>
2022-03-15 16:05:15 +0800

    [1-200] lc32: stack

--- golang/lc32LongestValidParentheses.go
+++ golang/lc32LongestValidParentheses.go
@@ -8,32 +8,30 @@ package golang
 
 // @lc code=start
 func longestValidParentheses(s string) int {
-	n := len(s)
-	dp := make([]int, n)
+	stack := []int{-1}
 	ans := 0
 	for i, char := range s {
-		if i > 0 && char == ')' {
-			if s[i-1] == '(' {
-				if i-2 >= 0 {
-					dp[i] = dp[i-2] + 2
-				} else {
-					dp[i] = 2
-				}
+		if char == '(' {
+			stack = append(stack, i)
+		} else {
+			if len(stack) == 1 && stack[0] == -1 {
+				stack[0] = i
+			} else if s[stack[len(stack)-1]] == '(' {
+				stack = stack[:len(stack)-1]
+				ans = lc32Max(ans, i-stack[len(stack)-1])
 			} else {
-				if i-dp[i-1] >= 1 && s[i-dp[i-1]-1] == '(' {
-					if i-dp[i-1] >= 2 {
-						dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
-					} else {
-						dp[i] = dp[i-1] + 2
-					}
-				}
-			}
-			if dp[i] > ans {
-				ans = dp[i]
+				stack = append(stack, i)
 			}
 		}
 	}
 	return ans
+}
+
+func lc32Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
