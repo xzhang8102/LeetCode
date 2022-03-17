cfc038a1b6ed8178a9fc04896cda301414dd50f5
zxw <xzhang305@gmail.com>
2022-03-17 12:18:20 +0800

    [1-200] lc41: array

--- golang/lc41FirstMissingPositive.go
+++ golang/lc41FirstMissingPositive.go
@@ -10,30 +10,16 @@ package golang
 func firstMissingPositive(nums []int) int {
 	n := len(nums)
 	for i := 0; i < n; i++ {
-		if nums[i] <= 0 {
-			nums[i] = n + 1
+		for nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
 	}
-	for i := 0; i < n; i++ {
-		num := lc41abs(nums[i])
-		// make sure element at the index in the range [1:n] is negative
-		if num <= n {
-			nums[num-1] = -lc41abs(nums[num-1])
-		}
-	}
-	for i := 0; i < n; i++ {
-		if nums[i] > 0 {
+	for i, num := range nums {
+		if i+1 != num {
 			return i + 1
 		}
 	}
 	return n + 1
-}
-
-func lc41abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
 }
 
 // @lc code=end
