f90c174b64ea854413bed3ea971efee88ec32f0d
zxw <xzhang305@gmail.com>
2022-03-17 13:59:30 +0800

    [1-200] lc101: iterative

--- golang/lc101SymmetricTree.go
+++ golang/lc101SymmetricTree.go
@@ -19,17 +19,39 @@ func isSymmetric(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	return lc101IsMirror(root.Left, root.Right)
-}
-
-func lc101IsMirror(p, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
+	q1, q2 := []*TreeNode{}, []*TreeNode{}
+	if root.Left != nil {
+		q1 = append(q1, root.Left)
 	}
-	if p == nil || q == nil || p.Val != q.Val {
-		return false
+	if root.Right != nil {
+		q2 = append(q2, root.Right)
 	}
-	return lc101IsMirror(p.Right, q.Left) && lc101IsMirror(p.Left, q.Right)
+	for len(q1) > 0 && len(q2) > 0 {
+		node1, node2 := q1[0], q2[0]
+		q1, q2 = q1[1:], q2[1:]
+		if node1.Val != node2.Val {
+			return false
+		}
+		if node1.Right == nil && node2.Left != nil {
+			return false
+		}
+		if node1.Left == nil && node2.Right != nil {
+			return false
+		}
+		if node1.Right != nil {
+			q1 = append(q1, node1.Right)
+		}
+		if node2.Left != nil {
+			q2 = append(q2, node2.Left)
+		}
+		if node1.Left != nil {
+			q1 = append(q1, node1.Left)
+		}
+		if node2.Right != nil {
+			q2 = append(q2, node2.Right)
+		}
+	}
+	return len(q1) == 0 && len(q2) == 0
 }
 
 // @lc code=end
