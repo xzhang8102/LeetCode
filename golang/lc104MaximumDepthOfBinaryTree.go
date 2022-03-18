c0165ca15f17e8def9953577af3d2449e38931d4
zxw <xzhang305@gmail.com>
2022-03-18 16:16:31 +0800

    [1-200] lc104: bfs

--- golang/lc104MaximumDepthOfBinaryTree.go
+++ golang/lc104MaximumDepthOfBinaryTree.go
@@ -19,7 +19,24 @@ func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	return 1 + lc104Max(maxDepth(root.Left), maxDepth(root.Right))
+	ans := 0
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		qSize := len(q)
+		for qSize > 0 {
+			node := q[0]
+			q = q[1:]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+			qSize--
+		}
+		ans++
+	}
+	return ans
 }
 
 func lc104Max(a, b int) int {
