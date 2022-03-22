e7ff3830699225e7c8f688f7096581889e28cb43
zxw <xzhang305@gmail.com>
2022-03-22 23:01:08 +0800

    [1-200] lc84: mono stack (one traversal)

--- golang/lc84LargestRectangleInHistogram.go
+++ golang/lc84LargestRectangleInHistogram.go
@@ -7,13 +7,13 @@ package golang
  */
 
 // @lc code=start
-
 func largestRectangleArea(heights []int) int {
 	n := len(heights)
-	left := make([]int, n)
+	left, right := make([]int, n), make([]int, n)
 	stack := []int{}
 	for i := 0; i < n; i++ {
 		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			right[stack[len(stack)-1]] = i
 			stack = stack[:len(stack)-1]
 		}
 		if len(stack) == 0 {
@@ -23,18 +23,8 @@ func largestRectangleArea(heights []int) int {
 		}
 		stack = append(stack, i)
 	}
-	right := make([]int, n)
-	stack = []int{}
-	for i := n - 1; i >= 0; i-- {
-		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
-			stack = stack[:len(stack)-1]
-		}
-		if len(stack) == 0 {
-			right[i] = n
-		} else {
-			right[i] = stack[len(stack)-1]
-		}
-		stack = append(stack, i)
+	for _, idx := range stack {
+		right[idx] = n
 	}
 	ans := 0
 	for i := 0; i < n; i++ {
