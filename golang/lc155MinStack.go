35846dc39c6cab33e3d5ff3367a8240f5fd71b45
zxw <xzhang305@gmail.com>
2022-03-22 11:19:06 +0800

    [1-200] lc155: stack (with no extra stack)

--- golang/lc155MinStack.go
+++ golang/lc155MinStack.go
@@ -1,7 +1,5 @@
 package golang
 
-import "math"
-
 /*
  * @lc app=leetcode.cn id=155 lang=golang
  *
@@ -10,37 +8,43 @@ import "math"
 
 // @lc code=start
 type MinStack struct {
-	minStack []int
-	data     []int
+	stack []int
+	min   int
 }
 
 func Constructor() MinStack {
 	return MinStack{
-		[]int{math.MaxInt64},
 		[]int{},
+		0,
 	}
 }
 
 func (this *MinStack) Push(val int) {
-	this.data = append(this.data, val)
-	if min := this.minStack[len(this.minStack)-1]; val <= min {
-		this.minStack = append(this.minStack, val)
+	if len(this.stack) == 0 {
+		this.min = val
+	}
+	this.stack = append(this.stack, val)
+	if val < this.min {
+		this.stack = append(this.stack, this.min-val)
+		this.min = val
 	} else {
-		this.minStack = append(this.minStack, min)
+		this.stack = append(this.stack, 0)
 	}
 }
 
 func (this *MinStack) Pop() {
-	this.data = this.data[:len(this.data)-1]
-	this.minStack = this.minStack[:len(this.minStack)-1]
+	if diff := this.stack[len(this.stack)-1]; diff > 0 {
+		this.min = this.stack[len(this.stack)-2] + diff
+	}
+	this.stack = this.stack[:len(this.stack)-2]
 }
 
 func (this *MinStack) Top() int {
-	return this.data[len(this.data)-1]
+	return this.stack[len(this.stack)-2]
 }
 
 func (this *MinStack) GetMin() int {
-	return this.minStack[len(this.minStack)-1]
+	return this.min
 }
 
 /**
