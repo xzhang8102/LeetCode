f2325aa7632b60b4a3bce8e0cb6b29756965998e
zxw <xzhang305@gmail.com>
2022-03-24 14:20:40 +0800

    [dp] lc801: dp

--- golang/lc801MinimumSwapsToMakeSequencesIncreasing.go
+++ golang/lc801MinimumSwapsToMakeSequencesIncreasing.go
@@ -7,29 +7,25 @@ package golang
  */
 
 // @lc code=start
-import "math"
-
 func minSwap(nums1 []int, nums2 []int) int {
 	n := len(nums1)
-	// dp[i][0] 代表nums1[0->i], nums2[0->i]均为单调递增且index i不交换的最小swap数
-	// dp[i][1] 代表nums1[0->i], nums2[0->i]均为单调递增且index i交换的最小swap数
+	// dp[i][0] how many swaps so far if we do not swap at index i
+	// dp[i][1] how many swaps so far if we swap at index i
 	dp := make([][2]int, n)
-	for i := range dp {
-		dp[i][0] = math.MaxInt32
-		dp[i][1] = math.MaxInt32
-	}
-	dp[0][0] = 0
 	dp[0][1] = 1
 	for i := 1; i < n; i++ {
 		if nums1[i] > nums1[i-1] && nums2[i] > nums2[i-1] {
-			dp[i][0] = dp[i-1][0]
-			// 如果已经是单调递增，必须交换已确保还是单调递增(因为真实数组并没有进行变动)
-			dp[i][1] = dp[i-1][1] + 1
-		}
-		// 交叉检查 对于局部情况交叉检查就相当于真实交换
-		if nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1] {
-			dp[i][0] = lc801Min(dp[i][0], dp[i-1][1])
-			dp[i][1] = lc801Min(dp[i][1], dp[i-1][0]+1)
+			if nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1] {
+				// does not matter if swapped
+				dp[i][0] = lc801Min(dp[i-1][0], dp[i-1][1])
+				dp[i][1] = dp[i][0] + 1
+			} else {
+				dp[i][0] = dp[i-1][0]
+				dp[i][1] = dp[i-1][1] + 1
+			}
+		} else if nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1] {
+			dp[i][0] = dp[i-1][1]
+			dp[i][1] = dp[i-1][0] + 1
 		}
 	}
 	return lc801Min(dp[n-1][0], dp[n-1][1])
