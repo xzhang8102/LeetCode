cedf15e4db3fa7b57021ab242f7f28c8de18807c
zxw <xzhang305@gmail.com>
2022-03-24 12:13:13 +0800

    [dp] lc790: dp (optimized space)

--- golang/lc790DominoAndTrominoTiling.go
+++ golang/lc790DominoAndTrominoTiling.go
@@ -9,15 +9,21 @@ package golang
 // @lc code=start
 func numTilings(n int) int {
 	const MOD = 1e9 + 7
-	dp := make([][3]int, n+1)
-	dp[0][0] = 1
-	dp[1][0] = 1
-	for i := 2; i <= n; i++ {
-		dp[i][0] = (dp[i-1][0] + dp[i-2][0] + dp[i-1][1] + dp[i-1][2]) % MOD
-		dp[i][1] = (dp[i-2][0] + dp[i-1][2]) % MOD
-		dp[i][2] = (dp[i-2][0] + dp[i-1][1]) % MOD
+	// init: when n == 1
+	// dp[0] curr col is fully covered
+	// dp[1] curr col is covered by its first row
+	// dp[2] curr col is covered by its second row
+	// dp[3] curr col is empty and prev col is fully covered
+	dp := [4]int{1, 0, 0, 1}
+	for i := 1; i < n; i++ {
+		tmp := [4]int{}
+		tmp[0] = (dp[0] + dp[1] + dp[2] + dp[3]) % MOD
+		tmp[1] = (dp[3] + dp[2]) % MOD
+		tmp[2] = (dp[3] + dp[1]) % MOD
+		tmp[3] = dp[0]
+		dp = tmp
 	}
-	return dp[n][0]
+	return dp[0]
 }
 
 // @lc code=end
