ee754ea4c29600b6a1f80fb6aefa595d96b07a4f
zxw <xzhang305@gmail.com>
2022-03-25 12:56:07 +0800

    [daily] lc172: add comment

--- golang/lc172FactorialTrailingZeroes.go
+++ golang/lc172FactorialTrailingZeroes.go
@@ -11,6 +11,11 @@ func trailingZeroes(n int) int {
 	if n < 5 {
 		return 0
 	}
+	// 计算从0 ~ n共有多少个 5^n 的倍数
+	// target = 0 + (5 ^ n) * cnt
+	// 以130举例，
+	// 第一次除以5，得到1-130中有多少个5的倍数
+	// 第二次除以5，得到1-130中有多少个25的倍数
 	return n/5 + trailingZeroes(n/5)
 }
 
