36eddbf3a5a8bc74821675930040acbd426b43e0
zxw <xzhang305@gmail.com>
2022-03-26 17:12:15 +0800

    [medium] lc63: dp (optimized space)

--- golang/lc63UniquePathsIi.go
+++ golang/lc63UniquePathsIi.go
@@ -13,39 +13,22 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 		return 0
 	}
 	col := len(obstacleGrid[0])
-	dp := make([][]int, row)
-	for i := range obstacleGrid {
-		dp[i] = make([]int, col)
-		if i == 0 {
-			dp[0][0] = 1
-			if obstacleGrid[0][0] == 1 {
-				dp[0][0] = 0
-			}
-			for j := 1; j < col; j++ {
-				if obstacleGrid[0][j] == 1 {
-					dp[0][j] = 0
-				} else {
-					dp[0][j] = dp[0][j-1]
-				}
-			}
-		} else {
-			if obstacleGrid[i][0] == 1 {
-				dp[i][0] = 0
-			} else {
-				dp[i][0] = dp[i-1][0]
-			}
-		}
+	dp := make([]int, col)
+	if obstacleGrid[0][0] == 0 {
+		dp[0] = 1
 	}
-	for r := 1; r < row; r++ {
-		for c := 1; c < col; c++ {
+	for r := 0; r < row; r++ {
+		for c := 0; c < col; c++ {
 			if obstacleGrid[r][c] == 1 {
-				dp[r][c] = 0
-			} else {
-				dp[r][c] = dp[r-1][c] + dp[r][c-1]
+				dp[c] = 0
+				continue
+			}
+			if c > 0 {
+				dp[c] += dp[c-1]
 			}
 		}
 	}
-	return dp[row-1][col-1]
+	return dp[col-1]
 }
 
 // @lc code=end
