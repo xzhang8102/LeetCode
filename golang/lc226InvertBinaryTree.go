71ae46525d7e3e10e51ac3983191e60d137d53fe
zxw <xzhang305@gmail.com>
2022-03-27 22:00:53 +0800

    [easy] lc226: recursive

---
+++ golang/lc226InvertBinaryTree.go
@@ -0,0 +1,28 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+// @lc code=end
