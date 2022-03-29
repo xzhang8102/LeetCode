09f4637a5e075909c0b53b47dfc09461caced3b4
zxw <xzhang305@gmail.com>
2022-03-29 11:58:06 +0800

    [hard] lc115: dp (1-d array for state)

--- golang/lc115DistinctSubsequences.go
+++ golang/lc115DistinctSubsequences.go
@@ -9,25 +9,22 @@ package golang
 // @lc code=start
 func numDistinct(s string, t string) int {
 	n, m := len(s), len(t)
-	dp := make([][]int, m+1)
+	dp := make([]int, n+1)
 	for i := range dp {
-		dp[i] = make([]int, n+1)
-		if i == 0 {
-			for j := range dp[i] {
-				dp[i][j] = 1
-			}
-		}
+		dp[i] = 1
 	}
-	for i := 1; i <= m; i++ {
+	for i := 0; i < m; i++ {
+		tmp := make([]int, n+1)
 		for j := 1; j <= n; j++ {
-			if s[j-1] == t[i-1] {
-				dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
+			if s[j-1] == t[i] {
+				tmp[j] = dp[j-1] + tmp[j-1]
 			} else {
-				dp[i][j] = dp[i][j-1]
+				tmp[j] = tmp[j-1]
 			}
 		}
+		dp = tmp
 	}
-	return dp[m][n]
+	return dp[n]
 }
 
 // @lc code=end
