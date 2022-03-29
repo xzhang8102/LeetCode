4a1dd9816ec0dd8a39e6821c1769b55ff093eb5b
zxw <xzhang305@gmail.com>
2022-03-30 07:33:07 +0800

    [easy] lc263: math

---
+++ golang/lc263UglyNumber.go
@@ -0,0 +1,26 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=263 lang=golang
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+func isUgly(n int) bool {
+	if n == 0 {
+		return false
+	}
+	for n%2 == 0 {
+		n /= 2
+	}
+	for n%3 == 0 {
+		n /= 3
+	}
+	for n%5 == 0 {
+		n /= 5
+	}
+	return n == 1
+}
+
+// @lc code=end
