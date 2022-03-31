2d0133b1bf858e7bd464809740efd95ceef57044
zxw <xzhang305@gmail.com>
2022-03-31 11:33:26 +0800

    [medium] lc97: dp (1-d array as extra space)

--- golang/lc97InterleavingString.go
+++ golang/lc97InterleavingString.go
@@ -12,30 +12,24 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	if n3 != n1+n2 {
 		return false
 	}
-	dp := make([][]bool, n2+1)
-	for i := range dp {
-		dp[i] = make([]bool, n1+1)
-		if i == 0 {
-			dp[0][0] = true
-			for j := 1; j <= n1; j++ {
-				dp[0][j] = s3[j-1] == s1[j-1] && dp[0][j-1]
-			}
-		} else {
-			dp[i][0] = s3[i-1] == s2[i-1] && dp[i-1][0]
-		}
+	dp := make([]bool, n1+1)
+	dp[0] = true
+	for j := 1; j <= n1; j++ {
+		dp[j] = dp[j-1] && s3[j-1] == s1[j-1]
 	}
-	for p2 := 1; p2 <= n2; p2++ {
-		for p1 := 1; p1 <= n1; p1++ {
-			p3 := p2 + p1 - 1
-			if dp[p2][p1-1] {
-				dp[p2][p1] = s3[p3] == s1[p1-1]
+	for i := 1; i <= n2; i++ {
+		dp[0] = dp[0] && s3[i-1] == s2[i-1]
+		for j := 1; j <= n1; j++ {
+			p3 := i + j - 1
+			if dp[j] {
+				dp[j] = s3[p3] == s2[i-1]
 			}
-			if dp[p2-1][p1] {
-				dp[p2][p1] = dp[p2][p1] || s3[p3] == s2[p2-1]
+			if dp[j-1] {
+				dp[j] = dp[j] || s3[p3] == s1[j-1]
 			}
 		}
 	}
-	return dp[len(s2)][len(s1)]
+	return dp[n1]
 }
 
 // @lc code=end
