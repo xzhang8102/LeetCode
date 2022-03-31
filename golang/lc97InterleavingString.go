158ab3bfe2b73df9a44897a0ba6946c083a85a7d
zxw <xzhang305@gmail.com>
2022-03-31 10:27:29 +0800

    [medium] lc97: dfs (with memoization)

--- golang/lc97InterleavingString.go
+++ golang/lc97InterleavingString.go
@@ -8,38 +8,41 @@ package golang
 
 // @lc code=start
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	valid := false
-	if len(s3) != len(s1)+len(s2) {
+	n1, n2, n3 := len(s1), len(s2), len(s3)
+	if n3 != n1+n2 {
 		return false
 	}
-	var backtrack func(int, int, int, int) bool
-	backtrack = func(p1, p2, p3, turn int) bool {
-		if valid {
-			return true
-		}
-		if p1 == len(s1) && p2 == len(s2) && p3 == len(s3) {
-			return true
-		}
-		if turn == 1 {
-			for i := 0; p1+i < len(s1) && p3+i < len(s3) && s1[p1+i] == s3[p3+i]; i++ {
-				if !valid {
-					if backtrack(p1+i+1, p2, p3+i+1, -turn) {
-						valid = true
-					}
-				}
-			}
-		} else {
-			for i := 0; p2+i < len(s2) && p3+i < len(s3) && s2[p2+i] == s3[p3+i]; i++ {
-				if !valid {
-					if backtrack(p1, p2+i+1, p3+i+1, -turn) {
-						valid = true
-					}
-				}
-			}
-		}
-		return valid
+	dp := make([][]int, n2+1)
+	for i := range dp {
+		dp[i] = make([]int, n1+1)
 	}
-	return backtrack(0, 0, 0, 1) || backtrack(0, 0, 0, -1)
+	var dfs func(int, int, int)
+	dfs = func(p1, p2, p3 int) {
+		if p1 == len(s1) && p2 == len(s2) && p3 == len(s3) {
+			dp[p2][p1] = 1
+			return
+		}
+		if dp[p2][p1] != 0 {
+			return
+		}
+		if p1 < len(s1) && s1[p1] == s3[p3] {
+			dfs(p1+1, p2, p3+1)
+			if dp[p2][p1+1] == 1 {
+				dp[p2][p1] = 1
+			}
+		}
+		if p2 < len(s2) && s2[p2] == s3[p3] {
+			dfs(p1, p2+1, p3+1)
+			if dp[p2+1][p1] == 1 {
+				dp[p2][p1] = 1
+			}
+		}
+		if dp[p2][p1] != 1 {
+			dp[p2][p1] = -1
+		}
+	}
+	dfs(0, 0, 0)
+	return dp[len(s2)][len(s1)] == 1
 }
 
 // @lc code=end
