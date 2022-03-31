afd529e0c1f17b7832323b6c866024ed0b908dd2
zxw <xzhang305@gmail.com>
2022-03-31 20:46:33 +0800

    [hard] lc123: dp (optimized space)

--- golang/lc123BestTimeToBuyAndSellStockIii.go
+++ golang/lc123BestTimeToBuyAndSellStockIii.go
@@ -9,19 +9,23 @@ package golang
 // @lc code=start
 func maxProfit(prices []int) int {
 	n := len(prices)
-	dp := make([][2][3]int, n)
+	// dp := make([][2][3]int, n)
 	// dp[i][j][k] - the ith day; current share 0/1; trade counts 0/1/2
-	dp[0][0][1] = 0
-	dp[0][0][2] = 0
-	dp[0][1][0] = -prices[0]
-	dp[0][1][1] = -prices[0]
+	sell1 := 0         // dp[0][0][1] = 0
+	sell2 := 0         // dp[0][0][2] = 0
+	buy1 := -prices[0] // dp[0][1][0] = -prices[0]
+	buy2 := -prices[0] // dp[0][1][1] = -prices[0]
 	for i := 1; i < n; i++ {
-		dp[i][0][1] = lc123Max(dp[i-1][0][1], dp[i-1][1][0]+prices[i])
-		dp[i][0][2] = lc123Max(dp[i-1][0][2], dp[i-1][1][1]+prices[i])
-		dp[i][1][0] = lc123Max(dp[i-1][1][0], -prices[i])
-		dp[i][1][1] = lc123Max(dp[i-1][1][1], dp[i-1][0][1]-prices[i])
+		// dp[i][0][1] = lc123Max(dp[i-1][0][1], dp[i-1][1][0]+prices[i])
+		// dp[i][0][2] = lc123Max(dp[i-1][0][2], dp[i-1][1][1]+prices[i])
+		// dp[i][1][0] = lc123Max(dp[i-1][1][0], -prices[i])
+		// dp[i][1][1] = lc123Max(dp[i-1][1][1], dp[i-1][0][1]-prices[i])
+		buy1 = lc123Max(buy1, -prices[i])
+		sell1 = lc123Max(sell1, buy1+prices[i])
+		buy2 = lc123Max(buy2, sell1-prices[i])
+		sell2 = lc123Max(sell2, buy2+prices[i])
 	}
-	return lc123Max(0, lc123Max(dp[n-1][0][1], dp[n-1][0][2]))
+	return sell2
 }
 
 func lc123Max(a, b int) int {
