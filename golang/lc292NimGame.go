95a0e3646aa15ecd0d8577336cc288079342a8b3
zxw <xzhang305@gmail.com>
2022-04-01 13:06:05 +0800

    [easy] lc292: math

---
+++ golang/lc292NimGame.go
@@ -0,0 +1,14 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=292 lang=golang
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+func canWinNim(n int) bool {
+	return n%4 > 0
+}
+
+// @lc code=end
