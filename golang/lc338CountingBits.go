3f84cf33b801f85e795ae952c2ebf4796cacdb77
zxw <xzhang305@gmail.com>
2022-04-02 16:59:33 +0800

    [easy] lc338: dp

--- golang/lc338CountingBits.go
+++ golang/lc338CountingBits.go
@@ -9,12 +9,8 @@ package golang
 // @lc code=start
 func countBits(n int) []int {
 	ans := make([]int, n+1)
-	highest := 0
 	for i := 1; i <= n; i++ {
-		if i&(i-1) == 0 {
-			highest = i
-		}
-		ans[i] = ans[i-highest] + 1
+		ans[i] = ans[i>>1] + (i & 1)
 	}
 	return ans
 }
