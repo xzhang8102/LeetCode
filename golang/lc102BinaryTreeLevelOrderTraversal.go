e5dd0c50f9cbb1ab07c515e2153e4b19735eefb1
zxw <xzhang305@gmail.com>
2022-04-02 17:07:45 +0800

    [medium] lc102: bst bfs

---
+++ golang/lc102BinaryTreeLevelOrderTraversal.go
@@ -0,0 +1,41 @@
+package golang
+
+/*
+ * @lc app=leetcode.cn id=102 lang=golang
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	ans := [][]int{}
+	level := 0
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		tmp := q
+		q = []*TreeNode{}
+		level++
+		for _, node := range tmp {
+			if node == nil {
+				continue
+			}
+			if level > len(ans) {
+				ans = append(ans, []int{})
+			}
+			ans[level-1] = append(ans[level-1], node.Val)
+			q = append(q, node.Left)
+			q = append(q, node.Right)
+		}
+	}
+	return ans
+}
+
+// @lc code=end
