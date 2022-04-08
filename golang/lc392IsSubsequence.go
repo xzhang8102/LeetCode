d7c381bac0017f358667f9b3c64d79ed91753708
zxw <xzhang305@gmail.com>
2022-04-08 10:54:11 +0800

    [easy] lc392: dp

--- golang/lc392IsSubsequence.go
+++ golang/lc392IsSubsequence.go
@@ -8,19 +8,27 @@ package golang
 
 // @lc code=start
 func isSubsequence(s string, t string) bool {
-	if len(s) == 0 {
-		return true
-	}
-	ptr := 0
-	for _, ch := range t {
-		if byte(ch) == s[ptr] {
-			ptr++
-			if ptr == len(s) {
-				return true
+	n, m := len(s), len(t)
+	dp := make([][26]int, m+1)
+	for i := m - 1; i >= 0; i-- {
+		for j := 0; j < 26; j++ {
+			if t[i] == byte(j+'a') {
+				dp[i][j] = i
+			} else {
+				dp[i][j] = dp[i+1][j]
 			}
 		}
 	}
-	return false
+	i, next := 0, 0
+	for i < n && next < m {
+		next = dp[next][s[i]-'a']
+		if s[i] != t[next] {
+			return false
+		}
+		next++
+		i++
+	}
+	return i == n
 }
 
 // @lc code=end
