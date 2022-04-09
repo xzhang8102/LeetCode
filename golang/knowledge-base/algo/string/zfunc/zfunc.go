d627f335236f41ff8706b2b60e6d2ea2c3a38c2e
zxw <xzhang305@gmail.com>
2022-04-09 16:47:43 +0800

    [z-algo] add ref

--- golang/knowledge-base/algo/string/zfunc/zfunc.go
+++ golang/knowledge-base/algo/string/zfunc/zfunc.go
@@ -1,6 +1,24 @@
 package zfunc
 
-// ref: https://oi-wiki.org/string/z-func/
+// ref:
+// https://oi-wiki.org/string/z-func/
+// https://github.com/EndlessCheng/codeforces-go/blob/master/copypasta/strings.go
+
+// func calcZ(s string) []int {
+// 	n := len(s)
+// 	z := make([]int, n)
+// 	for i, l, r := 1, 0, 0; i < n; i++ {
+// 		if i <= r {
+// 			z[i] = min(z[i-l], r-i+1)
+// 		}
+// 		for i+z[i] < n && s[z[i]] == s[i+z[i]] {
+// 			l, r = i, i+z[i]
+// 			z[i]++
+// 		}
+// 	}
+// 	z[0] = n
+// 	return z
+// }
 
 // calculate z-values
 // z-value: the length of the longest prefix of
