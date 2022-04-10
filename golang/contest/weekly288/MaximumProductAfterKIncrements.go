c8253add9ccf86b7ae4a8f1b5caa37f8d2c0c7fd
zxw <xzhang305@gmail.com>
2022-04-10 12:59:44 +0800

    [contest] optimize the original solution

--- golang/contest/weekly288/MaximumProductAfterKIncrements.go
+++ golang/contest/weekly288/MaximumProductAfterKIncrements.go
@@ -1,35 +1,43 @@
 package weekly288
 
 import (
-	"container/heap"
+	"math"
+	"sort"
 )
 
-type priorityQ []int
-
-func (pq priorityQ) Len() int           { return len(pq) }
-func (pq priorityQ) Less(i, j int) bool { return pq[i] < pq[j] }
-func (pq priorityQ) Swap(i, j int)      { pq[i], pq[j] = pq[j], pq[i] }
-func (pq *priorityQ) Pop() interface{} {
-	x := (*pq)[pq.Len()-1]
-	*pq = (*pq)[:pq.Len()-1]
-	return x
-}
-func (pq *priorityQ) Push(v interface{}) {
-	*pq = append(*pq, v.(int))
-}
-
 func maximumProduct(nums []int, k int) int {
-	pq := priorityQ(nums)
-	heap.Init(&pq)
-	for k > 0 {
-		pq[0]++
-		heap.Fix(&pq, 0)
-		k--
+	const MOD int = 1e9 + 7
+	sort.Ints(nums)
+	i := 1
+	for i <= len(nums) && k > 0 {
+		if i < len(nums) && nums[i] == nums[0] {
+			i++
+		} else {
+			gap := math.MaxInt64
+			if i < len(nums) {
+				gap = nums[i] - nums[0]
+			}
+			add := min(k/i, gap)
+			k -= add * i
+			for j := 0; j < i && (add > 0 || k > 0); j++ {
+				nums[j] += add
+				if add < gap && k > 0 {
+					nums[j]++
+					k--
+				}
+			}
+		}
 	}
 	ans := 1
-	mod := int(1e9 + 7)
-	for pq.Len() > 0 {
-		ans = (ans * pq.Pop().(int)) % mod
+	for _, v := range nums {
+		ans = ans * v % MOD
 	}
 	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
