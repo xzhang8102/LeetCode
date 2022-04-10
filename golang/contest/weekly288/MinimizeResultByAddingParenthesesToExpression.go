b76c2720d095ec0c8bba37bc5a6eb05ef9e39460
zxw <xzhang305@gmail.com>
2022-04-10 13:07:38 +0800

    [contest] optimize the original solution

--- golang/contest/weekly288/MinimizeResultByAddingParenthesesToExpression.go
+++ golang/contest/weekly288/MinimizeResultByAddingParenthesesToExpression.go
@@ -13,29 +13,20 @@ func minimizeResult(expression string) string {
 	arr := strings.Split(expression, "+")
 	left, right := arr[0], arr[1]
 	for i := 0; i < len(left); i++ {
+		n1 := 1
+		if i > 0 {
+			n1, _ = strconv.Atoi(left[:i])
+		}
+		n2, _ := strconv.Atoi(left[i:])
 		for j := 1; j <= len(right); j++ {
-			var n1, n2, n3, n4 int
-			if i == 0 {
-				n1 = 1
-			} else {
-				n1, _ = strconv.Atoi(left[:i])
-			}
-			n2, _ = strconv.Atoi(left[i:])
-			if j == len(right) {
-				n4 = 1
-			} else {
+			n4 := 1
+			if j < len(right) {
 				n4, _ = strconv.Atoi(right[j:])
 			}
-			n3, _ = strconv.Atoi(right[:j])
-			if n1*(n2+n3)*n4 < min {
-				min = n1 * (n2 + n3) * n4
-				ans = fmt.Sprintf("(%d+%d)", n2, n3)
-				if i > 0 {
-					ans = fmt.Sprintf("%d%s", n1, ans)
-				}
-				if j < len(right) {
-					ans = fmt.Sprintf("%s%d", ans, n4)
-				}
+			n3, _ := strconv.Atoi(right[:j])
+			if val := n1 * (n2 + n3) * n4; val < min {
+				min = val
+				ans = fmt.Sprintf("%s(%s+%s)%s", left[:i], left[i:], right[:j], right[j:])
 			}
 		}
 	}
