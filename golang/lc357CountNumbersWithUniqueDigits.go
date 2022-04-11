c56e3f690359afc5c93a12612ab793fa75c69a02
zxw <xzhang305@gmail.com>
2022-04-11 22:03:49 +0800

    [daily] lc357: dp; math

--- golang/lc357CountNumbersWithUniqueDigits.go
+++ golang/lc357CountNumbersWithUniqueDigits.go
@@ -11,13 +11,11 @@ func countNumbersWithUniqueDigits(n int) int {
 	if n == 0 {
 		return 1
 	}
-	dp := make([]int, n+1)
-	dp[0] = 1
-	dp[1] = 10
+	prev, curr := 1, 10
 	for i := 2; i <= n; i++ {
-		dp[i] = dp[i-1] + (dp[i-1]-dp[i-2])*(11-i)
+		prev, curr = curr, curr+(curr-prev)*(11-i)
 	}
-	return dp[n]
+	return curr
 }
 
 // @lc code=end
