316e235301b93aec7e27297ef6c1999b0e87b459
zxw <xzhang305@gmail.com>
2022-04-14 20:49:00 +0800

    [medium] lc114: find preprocessor

--- golang/lc114FlattenBinaryTreeToLinkedList.go
+++ golang/lc114FlattenBinaryTreeToLinkedList.go
@@ -16,25 +16,18 @@ package golang
  * }
  */
 func flatten(root *TreeNode) {
-	if root == nil {
-		return
-	}
-	stack := []*TreeNode{root}
-	var ptr *TreeNode
-	for len(stack) > 0 {
-		top := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-		if top.Right != nil {
-			stack = append(stack, top.Right)
-		}
-		if top.Left != nil {
-			stack = append(stack, top.Left)
-		}
-		if ptr != nil {
+	ptr := root
+	for ptr != nil {
+		if ptr.Left != nil {
+			rightmost := ptr.Left
+			for rightmost.Right != nil {
+				rightmost = rightmost.Right
+			}
+			rightmost.Right = ptr.Right
+			ptr.Right = ptr.Left
 			ptr.Left = nil
-			ptr.Right = top
 		}
-		ptr = top
+		ptr = ptr.Right
 	}
 }
 
