161493a5c221fde7ae1f82f9cd044126d3f351b0
zxw <xzhang305@gmail.com>
2022-04-19 09:17:27 +0800

    [daily] lc821: cleanup

--- golang/lc821ShortestDistanceToACharacter.go
+++ golang/lc821ShortestDistanceToACharacter.go
@@ -1,7 +1,5 @@
 package golang
 
-import "math"
-
 /*
  * @lc app=leetcode.cn id=821 lang=golang
  *
@@ -12,29 +10,19 @@ import "math"
 func shortestToChar(s string, c byte) []int {
 	n := len(s)
 	ans := make([]int, n)
-	index := -1
+	index := -n
 	for i := 0; i < n; i++ {
-		switch s[i] {
-		case c:
+		if s[i] == c {
 			index = i
-		default:
-			if index == -1 {
-				ans[i] = math.MaxInt64
-			} else {
-				ans[i] = i - index
-			}
 		}
+		ans[i] = i - index
 	}
-	index = n
+	index = 2 * n
 	for i := n - 1; i >= 0; i-- {
-		switch s[i] {
-		case c:
+		if s[i] == c {
 			index = i
-		default:
-			if index < n {
-				ans[i] = lc821Min(index-i, ans[i])
-			}
 		}
+		ans[i] = lc821Min(ans[i], index-i)
 	}
 	return ans
 }
