56e23c065be1fdf17a8bb861af13337496d9b1e8
zxw <xzhang305@gmail.com>
2022-04-29 22:27:50 +0800

    minor update

--- golang/lc427ConstructQuadTree.go
+++ golang/lc427ConstructQuadTree.go
@@ -9,17 +9,17 @@ package golang
 /**
  * Definition for a QuadTree node.
  */
-type Node struct {
+type QuadTreeNode struct {
 	Val         bool
 	IsLeaf      bool
-	TopLeft     *Node
-	TopRight    *Node
-	BottomLeft  *Node
-	BottomRight *Node
+	TopLeft     *QuadTreeNode
+	TopRight    *QuadTreeNode
+	BottomLeft  *QuadTreeNode
+	BottomRight *QuadTreeNode
 }
 
 // @lc code=start
-func construct(grid [][]int) *Node {
+func construct(grid [][]int) *QuadTreeNode {
 	n := len(grid)
 	pre := make([][]int, n+1)
 	pre[0] = make([]int, n+1)
@@ -29,13 +29,13 @@ func construct(grid [][]int) *Node {
 			pre[i+1][j+1] = grid[i][j] + pre[i+1][j] + pre[i][j+1] - pre[i][j]
 		}
 	}
-	var dfs func(r0, r1, c0, c1 int) *Node
-	dfs = func(r0, r1, c0, c1 int) *Node {
+	var dfs func(r0, r1, c0, c1 int) *QuadTreeNode
+	dfs = func(r0, r1, c0, c1 int) *QuadTreeNode {
 		total := pre[r1][c1] - pre[r1][c0] - pre[r0][c1] + pre[r0][c0]
 		if total == 0 || total == (r1-r0)*(c1-c0) {
-			return &Node{Val: grid[r0][c0] == 1, IsLeaf: true}
+			return &QuadTreeNode{Val: grid[r0][c0] == 1, IsLeaf: true}
 		}
-		return &Node{
+		return &QuadTreeNode{
 			false,
 			false,
 			dfs(r0, (r0+r1)/2, c0, (c0+c1)/2),
