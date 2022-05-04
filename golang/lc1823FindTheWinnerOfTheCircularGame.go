efa7f20f416271db559a490c8e3aaa8844310844
zxw <xzhang305@gmail.com>
2022-05-04 17:21:08 +0800

    [daily] lc1823: josephus problem

--- golang/lc1823FindTheWinnerOfTheCircularGame.go
+++ golang/lc1823FindTheWinnerOfTheCircularGame.go
@@ -8,18 +8,11 @@ package golang
 
 // @lc code=start
 func findTheWinner(n int, k int) int {
-	players := make([]int, n)
-	for i := range players {
-		players[i] = i + 1
+	ans := 0
+	for i := 2; i <= n; i++ {
+		ans = (ans + k) % i
 	}
-	next := 0
-	for len(players) > 1 {
-		size := len(players)
-		next = (k + next - 1) % size
-		copy(players[next:size-1], players[next+1:])
-		players = players[:size-1]
-	}
-	return players[0]
+	return ans + 1
 }
 
 // @lc code=end
