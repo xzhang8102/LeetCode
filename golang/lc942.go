9cf013345573a0edd6b119643fb5158343238422
zxw <xzhang305@gmail.com>
2022-05-09 11:32:34 +0800

    [daily] lc942: greedy

--- golang/lc942.go
+++ golang/lc942.go
@@ -2,53 +2,17 @@ package golang
 
 func diStringMatch(s string) []int {
 	n := len(s)
-	picked := make([]bool, n+1)
-	found := false
-	var backtrack func(idx, prev int) []int
-	backtrack = func(idx, prev int) []int {
-		if idx == n {
-			found = true
-			return nil
-		}
-		var res []int
-		if idx == -1 {
-			for i := 0; i <= n; i++ {
-				picked[i] = true
-				tmp := backtrack(0, i)
-				if found {
-					res = append([]int{i}, tmp...)
-					return res
-				}
-				picked[i] = false
-			}
+	ans := make([]int, n+1)
+	lo, hi := 0, n
+	for i, ch := range s {
+		if ch == 'I' {
+			ans[i] = lo
+			lo++
 		} else {
-			if s[idx] == 'I' {
-				for i := prev + 1; i <= n; i++ {
-					if !picked[i] {
-						picked[i] = true
-						tmp := backtrack(idx+1, i)
-						if found {
-							res = append([]int{i}, tmp...)
-							return res
-						}
-						picked[i] = false
-					}
-				}
-			} else {
-				for i := prev - 1; i >= 0; i-- {
-					if !picked[i] {
-						picked[i] = true
-						tmp := backtrack(idx+1, i)
-						if found {
-							res = append([]int{i}, tmp...)
-							return res
-						}
-						picked[i] = false
-					}
-				}
-			}
+			ans[i] = hi
+			hi--
 		}
-		return res
 	}
-	return backtrack(-1, -1)
+	ans[n] = lo
+	return ans
 }
