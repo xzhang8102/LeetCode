4ee3cdfd6e0fed4d65d7f678d9dd525225c02b81
zxw <xzhang305@gmail.com>
2022-05-13 16:42:59 +0800

    [medium] lc204: math (sieve of Eratosthenes)

--- golang/lc204CountPrimes.go
+++ golang/lc204CountPrimes.go
@@ -8,22 +8,20 @@ package golang
 
 // @lc code=start
 func countPrimes(n int) int {
+	isPrime := make([]bool, n)
+	for i := range isPrime {
+		isPrime[i] = true
+	}
 	ans := 0
 	for i := 2; i < n; i++ {
-		if lc204IsPrime(i) {
+		if isPrime[i] {
 			ans++
+			for j := 2 * i; j < n; j += i {
+				isPrime[j] = false
+			}
 		}
 	}
 	return ans
-}
-
-func lc204IsPrime(x int) bool {
-	for i := 2; i*i <= x; i++ {
-		if x%i == 0 {
-			return false
-		}
-	}
-	return true
 }
 
 // @lc code=end
