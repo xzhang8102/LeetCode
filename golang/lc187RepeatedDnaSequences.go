4f3d0a4a7194146356b43d6007efb3e697243005
zxw <xzhang305@gmail.com>
2022-05-13 10:33:00 +0800

    [medium] lc187: hashmap; binary encoding

--- golang/lc187RepeatedDnaSequences.go
+++ golang/lc187RepeatedDnaSequences.go
@@ -9,14 +9,26 @@ package golang
 // @lc code=start
 func findRepeatedDnaSequences(s string) []string {
 	n := len(s)
+	if n < 10 {
+		return nil
+	}
+	mapping := map[byte]int{'A': 0x00, 'C': 0x01, 'G': 0x02, 'T': 0x03}
+	cnt := map[int]int{}
+	num := 0
 	ans := []string{}
-	set := map[string]int{}
-	for i := 0; i < n-9; i++ {
-		sub := s[i : i+10]
-		if set[sub] == 1 {
-			ans = append(ans, sub)
+	for left, right := 0, 0; right <= n; right++ {
+		if right-left < 10 {
+			num = num<<2 | mapping[s[right]]
+		} else {
+			cnt[num]++
+			if cnt[num] == 2 {
+				ans = append(ans, s[left:right])
+			}
+			if right < n {
+				num = (num<<2 | mapping[s[right]]) & 0xFFFFF
+			}
+			left++
 		}
-		set[sub]++
 	}
 	return ans
 }
