96b84cbec222b01ed564fd4cbfac3191b95f38b9
zxw <xzhang305@gmail.com>
2022-05-14 01:34:20 +0800

    [offer] p04: 2-d array

--- golang/others/offer/p04.go
+++ golang/others/offer/p04.go
@@ -1,7 +1,5 @@
 package offer
 
-import "sort"
-
 func findNumberIn2DArray(matrix [][]int, target int) bool {
 	n := len(matrix)
 	if n == 0 {
@@ -11,13 +9,14 @@ func findNumberIn2DArray(matrix [][]int, target int) bool {
 	if m == 0 {
 		return false
 	}
-	for i := 0; i < n; i++ {
-		if matrix[i][0] > target {
-			break
-		} else if matrix[i][m-1] >= target {
-			if idx := sort.SearchInts(matrix[i], target); matrix[i][idx] == target {
-				return true
-			}
+	i, j := 0, m-1
+	for i < n && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			i++
+		} else {
+			j--
 		}
 	}
 	return false
