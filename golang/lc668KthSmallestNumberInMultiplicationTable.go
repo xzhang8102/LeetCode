a703cd04060506c10a6db3069bef2c15e2a2cf3e
zxw <xzhang305@gmail.com>
2022-05-18 20:36:00 +0800

    [daily] lc668: binary search

--- golang/lc668KthSmallestNumberInMultiplicationTable.go
+++ golang/lc668KthSmallestNumberInMultiplicationTable.go
@@ -1,7 +1,5 @@
 package golang
 
-import "sort"
-
 /*
  * @lc app=leetcode.cn id=668 lang=golang
  *
@@ -10,13 +8,34 @@ import "sort"
 
 // @lc code=start
 func findKthNumber(m int, n int, k int) int {
-	return sort.Search(m*n+1, func(val int) bool {
-		cnt := val / n * n
-		for i := val/n + 1; i <= m; i++ {
-			cnt += val / i
+	lo, hi := 1, m*n
+	for lo < hi {
+		mid := lo + (hi-lo)>>1
+		if cnt := lc668GetCnt(m, n, mid); cnt >= k {
+			hi = mid
+		} else {
+			lo = mid + 1
 		}
-		return cnt >= k
-	})
+	}
+	return hi
+}
+
+func lc668GetCnt(m, n, val int) int {
+	ans := 0
+	for i := 1; i <= m; i++ {
+		if i > val {
+			break
+		}
+		ans += lc668Min(val/i, n)
+	}
+	return ans
+}
+
+func lc668Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
