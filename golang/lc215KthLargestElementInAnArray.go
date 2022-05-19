6869bbcd654ed02301c488120d41d15d360aef56
zxw <xzhang305@gmail.com>
2022-05-19 16:00:24 +0800

    [medium] lc215: heap sort

--- golang/lc215KthLargestElementInAnArray.go
+++ golang/lc215KthLargestElementInAnArray.go
@@ -1,10 +1,5 @@
 package golang
 
-import (
-	"math/rand"
-	"time"
-)
-
 /*
  * @lc app=leetcode.cn id=215 lang=golang
  *
@@ -13,34 +8,35 @@ import (
 
 // @lc code=start
 func findKthLargest(nums []int, k int) int {
-	rand.Seed(time.Now().UnixNano())
-	return lc215QuickSelect(nums, 0, len(nums)-1, len(nums)-k)
-}
-
-func lc215QuickSelect(nums []int, start, end, target int) int {
-	idx := lc215RandomPartition(nums, start, end)
-	if idx == target {
-		return nums[idx]
-	} else if idx < target {
-		return lc215QuickSelect(nums, idx+1, end, target)
-	} else {
-		return lc215QuickSelect(nums, start, idx-1, target)
+	n := len(nums)
+	for i := n / 2; i >= 0; i-- {
+		lc215MaxHeapify(nums, i, n)
 	}
+	i := n - 1
+	for i > n-k {
+		nums[0], nums[i] = nums[i], nums[0]
+		lc215MaxHeapify(nums, 0, i)
+		i--
+	}
+	return nums[0]
 }
 
-func lc215RandomPartition(nums []int, start, end int) int {
-	idx := rand.Int()%(end-start+1) + start
-	nums[idx], nums[end] = nums[end], nums[idx]
-	val := nums[end]
-	i := start - 1
-	for j := start; j < end; j++ {
-		if nums[j] <= val {
-			i++
-			nums[i], nums[j] = nums[j], nums[i]
+func lc215MaxHeapify(nums []int, lo, hi int) {
+	root := lo
+	for {
+		child := 2*root + 1
+		if child >= hi {
+			break
 		}
+		if child+1 < hi && nums[child+1] > nums[child] {
+			child++
+		}
+		if nums[root] >= nums[child] {
+			return
+		}
+		nums[root], nums[child] = nums[child], nums[root]
+		root = child
 	}
-	nums[i+1], nums[end] = nums[end], nums[i+1]
-	return i + 1
 }
 
 // @lc code=end
