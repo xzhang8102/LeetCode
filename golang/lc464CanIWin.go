b9e2932c5a28a8bcdaef624e5220b7350070eae7
zxw <xzhang305@gmail.com>
2022-05-22 21:36:18 +0800

    [daily] lc464: dfs; memoization

--- golang/lc464CanIWin.go
+++ golang/lc464CanIWin.go
@@ -1,7 +1,5 @@
 package golang
 
-import "math"
-
 /*
  * @lc app=leetcode.cn id=464 lang=golang
  *
@@ -10,50 +8,28 @@ import "math"
 
 // @lc code=start
 func canIWin(maxChoosableInteger int, desiredTotal int) bool {
-	if desiredTotal == 0 {
-		return true
-	}
 	if desiredTotal > (1+maxChoosableInteger)*maxChoosableInteger/2 {
 		return false
 	}
-	used := 0
-	state := [2][]int{}
-	state[0] = make([]int, int(math.Pow(2, float64(maxChoosableInteger))))
-	state[1] = make([]int, int(math.Pow(2, float64(maxChoosableInteger))))
-	var dfs func(turn, total int) bool
-	dfs = func(turn, total int) bool {
-		if state[turn&1][used] != 0 {
-			return state[turn&1][used] == 1
+	dp := make([]int8, 1<<maxChoosableInteger)
+	for i := range dp {
+		dp[i] = -1
+	}
+	var dfs func(int, int) int8
+	dfs = func(usedNum, total int) (res int8) {
+		if dp[usedNum] != -1 {
+			return dp[usedNum]
 		}
-		if total >= desiredTotal {
-			return (turn-1)&1 == 0
-		}
-		state[turn&1][used] = 2
-		if turn&1 == 1 {
-			state[1][used] = 1
-		}
+		defer func() { dp[usedNum] = res }()
 		for i := 0; i < maxChoosableInteger; i++ {
-			if (used>>i)&1 == 0 {
-				used ^= 1 << i
-				if turn&1 == 0 {
-					if dfs(turn+1, total+i+1) {
-						used ^= 1 << i
-						state[0][used] = 1
-						break
-					}
-				} else {
-					if !dfs(turn+1, total+i+1) {
-						used ^= 1 << i
-						state[1][used] = 2
-						break
-					}
-				}
-				used ^= 1 << i
+			if usedNum>>i&1 == 0 && (total+i+1 >= desiredTotal || dfs(usedNum|1<<i, total+i+1) == 0) {
+				res = 1
+				return
 			}
 		}
-		return state[turn&1][used] == 1
+		return
 	}
-	return dfs(0, 0)
+	return dfs(0, 0) == 1
 }
 
 // @lc code=end
