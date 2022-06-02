d56555724cd1d1199f7cc7bc710c85879dd3ffe7
zxw <xzhang305@gmail.com>
2022-06-02 14:13:43 +0800

    [daily] lc473: dp

--- golang/lc473MatchsticksToSquare.go
+++ golang/lc473MatchsticksToSquare.go
@@ -1,7 +1,5 @@
 package golang
 
-import "sort"
-
 /*
  * @lc app=leetcode.cn id=473 lang=golang
  *
@@ -22,33 +20,23 @@ func makesquare(matchsticks []int) bool {
 		return false
 	}
 	edge := totalLen / 4
-	sort.Slice(matchsticks, func(i, j int) bool { return matchsticks[i] >= matchsticks[j] })
-	found := false
-	pick := [4]int{}
-	visited := map[[4]int]bool{}
-	var backtrack func(start int)
-	backtrack = func(start int) {
-		if found {
-			return
-		}
-		if visited[pick] {
-			return
-		}
-		visited[pick] = true
-		if start == n {
-			found = true
-			return
-		}
-		for i := 0; i < 4; i++ {
-			if pick[i]+matchsticks[start] <= edge {
-				pick[i] += matchsticks[start]
-				backtrack(start + 1)
-				pick[i] -= matchsticks[start]
+	dp := make([]int, 1<<n)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = -1
+	}
+	for state := 1; state < len(dp); state++ {
+		for i, match := range matchsticks {
+			if state>>i&1 == 0 {
+				continue
+			}
+			prev := state ^ (1 << i)
+			if dp[prev] >= 0 && dp[prev]+match <= edge {
+				dp[state] = (dp[prev] + match) % edge
+				break
 			}
 		}
 	}
-	backtrack(0)
-	return found
+	return dp[len(dp)-1] == 0
 }
 
 // @lc code=end
